37fbd0f4f3971d37572349826de62d617769262a
sebastianlofaro <sebastianlofaro@me.com>
2015-12-30 17:25:55 -0600

    Ingredients button
    Trying to add the ingredients button.

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 $(document).ready( function() {
+
+
 	$('.searchButton').on("click", function() {
 		var keyword = $(".textInput").val();
 		console.log(keyword);
@@ -12,18 +14,48 @@ $(document).ready( function() {
 		$('.resultsContainer').html("");
 		$.each(result.matches, function(key, value) {
 			console.log(value.totalTimeInSeconds);
-			var output = '<div class="result"><a href="https://www.yummly.com/recipe/' + value.id + '"><h3 class="recipeName">' + value.sourceDisplayName + '</h3></a><img src="' + value.smallImageUrls[0] + '" class="recipeImage"><p>Cooking time: ' + (value.totalTimeInSeconds/60) + ' minutes</p><p>Rating: ' + value.rating + ' </p></div>';
+			var output = '<div class="result"><a href="https://www.yummly.com/recipe/' + value.id + '"><h3 class="recipeName">' + value.sourceDisplayName + '</h3></a><img src="' + value.smallImageUrls[0] + '" class="recipeImage"><p>Cooking time: ' + (value.totalTimeInSeconds/60) + ' minutes</p><p>Rating: ' + value.rating + ' </p><input type="submit" class="ingredientsButton" placeholder="Ingredients"><ul class="ingredientList"><li>Milk</li><li>Eggs</li><li>Flour</li></ul></div>';
 			$('.resultsContainer').append(output);
+			$('.ingredientList').hide();
 		});
-
-		
-		
-
-
 	})
-	.fail(function(jqXHR, error, errorThrown){
-		
+	.fail(function(jqXHR, error, errorThrown){	
 	});
 	});
+
+	
+
 });
 
+$('.ingredientsButton').on("click", function() {
+		$('.ingredientList').show();
+		alert();
+	});
+
+
+
+//Test
+	//<div class="result"><a href="https://www.yummly.com/recipe/' + value.id + '"><h3 class="recipeName">' + value.sourceDisplayName + '</h3></a><img src="' + value.smallImageUrls[0] + '" class="recipeImage"><p>Cooking time: ' + (value.totalTimeInSeconds/60) + ' minutes</p><p>Rating: ' + value.rating + ' </p><div class="accordion"><h3>Ingredients</h3><div><ul class="ingredientList"></ul></div></div></div>';
+
+
+// Known Good
+//'<div class="result"><a href="https://www.yummly.com/recipe/' + value.id + '"><h3 class="recipeName">' + value.sourceDisplayName + '</h3></a><img src="' + value.smallImageUrls[0] + '" class="recipeImage"><p>Cooking time: ' + (value.totalTimeInSeconds/60) + ' minutes</p><p>Rating: ' + value.rating + ' </p></div>';
+
+
+
+/*	
+	
+'<div class="result">
+	<a href="https://www.yummly.com/recipe/' + value.id + '"><h3 class="recipeName">' + value.sourceDisplayName + '</h3></a>
+	<img src="' + value.smallImageUrls[0] + '" class="recipeImage">
+	<p>Cooking time: ' + (value.totalTimeInSeconds/60) + ' minutes</p>
+	<p>Rating: ' + value.rating + ' </p>
+	<input type="submit" class="ingredientsButton" placeholder="Ingredients">
+	<ul class="ingredientList">
+		<li>Milk</li>
+		<li>Eggs</li>
+		<li>Flour</li>
+	</ul>
+</div>';
+
+*/
